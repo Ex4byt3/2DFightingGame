aca51c82ed67796ac4f498f0f62922901d5a5df2
Chris B <cgchrisbeck@gmail.com>
2024-01-16 17:03:10 -0500

    Add Online Buttons

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -6,41 +6,67 @@ onready var training_tab = $TrainingTab
 onready var records_tab = $RecordsTab
 onready var quit_tab = $QuitTab
 
-onready var quit_desktop = $QuitTab/DesktopButton
+onready var rpc_connection_panel = $RPCConnectionPanel
+onready var steam_connection_panel = $SteamConnectionPanel
 
 func _ready():
-	#$MenuIcons/OnlineButton.grab_focus()
 	pass
 
 func _on_OnlineButton_toggled(button_pressed):
 	if button_pressed == true:
 		online_tab.visible = true
+		rpc_connection_panel.visible = false
+		steam_connection_panel.visible = false
 	else:
 		online_tab.visible = false
 
 func _on_LocalButton_toggled(button_pressed):
 	if button_pressed == true:
 		local_tab.visible = true
+		rpc_connection_panel.visible = false
+		steam_connection_panel.visible = false
 	else:
 		local_tab.visible = false
 
 func _on_TrainingButton_toggled(button_pressed):
 	if button_pressed == true:
 		training_tab.visible = true
+		rpc_connection_panel.visible = false
+		steam_connection_panel.visible = false
 	else:
 		training_tab.visible = false
 
 func _on_RecordsButton_toggled(button_pressed):
 	if button_pressed == true:
 		records_tab.visible = true
+		rpc_connection_panel.visible = false
+		steam_connection_panel.visible = false
 	else:
 		records_tab.visible = false
 
 func _on_QuitButton_toggled(button_pressed):
 	if button_pressed == true:
 		quit_tab.visible = true
+		rpc_connection_panel.visible = false
+		steam_connection_panel.visible = false
 	else:
 		quit_tab.visible = false
 
 func _on_DesktopButton_pressed():
 	get_tree().quit()
+
+func _on_RPCButton_toggled(button_pressed):
+	if button_pressed == true:
+		rpc_connection_panel.visible = true
+	else:
+		rpc_connection_panel.visible = false
+
+func _on_SteamButton_toggled(button_pressed):
+	if button_pressed == true:
+		steam_connection_panel.visible = true
+	else:
+		steam_connection_panel.visible = false
+
+func _on_SettingsButton_pressed():
+	OS.set_borderless_window(true)
+	OS.set_window_fullscreen(true)
