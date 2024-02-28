d3f885c75987d5e3d449751c1487798569ef9f89
ChrispyKing <cgchrisbeck@gmail.com>
2024-02-28 13:55:04 -0500

    Update Menu.gd

--- scripts/menu/Menu.gd
+++ scripts/menu/Menu.gd
@@ -3,7 +3,6 @@ extends Control
 
 # Onready variables for the various menus and header
 onready var main_menu = $MainMenu
-onready var settings_menu = $SettingsMenu
 onready var settings_overlay = $SettingsOverlay
 onready var online_menu = $OnlineMenu
 onready var menu_header = $MenuHeader
@@ -26,18 +25,25 @@ func _on_change_menu(menu: String) -> void:
 	match menu:
 		"MAIN": # Main menu
 			main_menu.visible = true
-			settings_menu.visible = false
 			online_menu.visible = false
-			
-		"SETTINGS": # Settings menu
+			# If hiding the settings overlay, save settings
 			if settings_overlay.visible:
 				settings_overlay.visible = false
-				# Send a signal to save the current settings
 				MenuSignalBus.emit_set_settings_dict(SettingsData.create_storage_dictionary())
-			else:
-				settings_overlay.visible = true
-			
+		
 		"ONLINE": # Online menu
 			main_menu.visible = false
-			settings_menu.visible = false
 			online_menu.visible = true
+			# If hiding the settings overlay, save settings
+			if settings_overlay.visible:
+				settings_overlay.visible = false
+				MenuSignalBus.emit_set_settings_dict(SettingsData.create_storage_dictionary())
+		
+		"SETTINGS": # Settings menu
+			# If hiding the settings overlay, save settings
+			if settings_overlay.visible:
+				settings_overlay.visible = false
+				MenuSignalBus.emit_set_settings_dict(SettingsData.create_storage_dictionary())
+			# Else show the overlay
+			else:
+				settings_overlay.visible = true
