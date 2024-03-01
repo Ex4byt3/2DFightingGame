0a32a072ec605235a88d25e01d1717187158cac3
Josh <jo853405@ucf.edu>
2024-03-01 00:42:10 -0500

    Add Dynamic Camera Adjustments
    --Refactored zoom_camera() to account for the x and y distance between players so that it can zoom accordingly.
    
    --Added smoothing to the camera position updates.

--- scripts/gameplay/StageCamera.gd
+++ scripts/gameplay/StageCamera.gd
@@ -1,9 +1,11 @@
 extends Camera2D
 
-export var zoom_scale: int = 1650 # Scales the distance of the players to a reasonable zooming value
-export var min_zoom: float = 0.7 # The distance between players before the camera stops zooming in
-export var max_zoom: float = 1.25 # Max zoom out value. Set to match the width of the stage.
-export var cam_y_pos: float = -93 # y value of the cam's position should be constant.
+export (float, 1, 5000) var zoom_scaleX = 1650 # Scales the X distance of the players to a reasonable zooming value.
+export (float, 1, 5000) var zoom_scaleY = 600 # Scales the Y distance of the players to a reasonable zooming value.
+export (float, 0.1, 5000) var min_zoomX: float = 1200 # The X distance between players before the camera stops zooming in.
+export (float, 0.1, 5000) var min_zoomY: float = 400 # The Y distance between players before the camera stops zooming in.
+export (float, 0.1, 10) var max_zoom: float = 1.25 # Max zoom out value. Set to match the width of the stage.
+export (float, -5000, 5000) var cam_y_pos: float = -93 # y value of the cam's position should be constant.
 var players = []
 
 onready var server_player = $"../ServerPlayer"
@@ -25,17 +27,18 @@ func update_position():
 	self.position = Vector2(playerAvg.x, cam_y_pos)
 
 func zoom_camera():
-	# Zoom camera based on the distance of the 2 furthest players. Accounts for min and max zoom settings.
-	# TODO: Add different min/max zooms for x and y distance.
-	var max_distance: int = 0
+	# Zoom camera based on the distance of the 2 furthest players. Accounts for min X/Y zoom settings.
+	var max_distanceX: float = 0
+	var max_distanceY: float = 0
 	for i in players:
 		for j in players:
 			if i==j: continue
-			var dist: int = (i.position - j.position).abs().length_squared()
-			max_distance = max(max_distance, dist)
-	if zoom_scale <= 0:
-		zoom_scale = 1
-	var zoom_amount = max(min_zoom, sqrt(max_distance) / zoom_scale)
-	zoom_amount = min(zoom_amount, max_zoom)
+			var distX: float = abs(i.position.x - j.position.x)
+			var distY: float = abs(i.position.y - j.position.y)
+			max_distanceX = max(distX, max_distanceX)
+			max_distanceY = max(distY, max_distanceY)
+	max_distanceX = max(min_zoomX, max_distanceX)
+	max_distanceY = max(min_zoomY, max_distanceY)
+	var zoom_amount:float = max(max_distanceX / zoom_scaleX, max_distanceY / zoom_scaleY)
+	zoom_amount = min(max_zoom, zoom_amount)
 	self.zoom = Vector2(zoom_amount, zoom_amount)
-	print(self.zoom)
