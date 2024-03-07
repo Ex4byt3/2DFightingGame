9ce2bdb9b25307a05988d5803532a35f5f5773d1
Josh <jo853405@ucf.edu>
2024-03-07 02:50:59 -0500

    Port Dynamic Camera
    --Reworked Dynamic Camera to work with Godot 4.

--- scripts/gameplay/StageCamera.gd
+++ scripts/gameplay/StageCamera.gd
@@ -1,22 +1,28 @@
 extends Camera2D
 
-var zoom_scaleX = 1650 # Scales the X distance of the players to a reasonable zooming value.
-var zoom_scaleY = 600 # Scales the Y distance of the players to a reasonable zooming value.
-var min_zoomX: float = 1200 # The X distance between players before the camera stops zooming in.
-var min_zoomY: float = 400 # The Y distance between players before the camera stops zooming in.
-var max_zoom: float = 1.25 # Max zoom out value. Set to match the width of the stage.
-var cam_y_pos: float = -93 # y value of the cam's position should be constant.
+@export var min_distX: float = 1200 # The X distance between players before the camera stops zooming in.
+@export var min_distY: float = 400 # The Y distance between players before the camera stops zooming in.
+@export var max_zoom: float = 0.8 # Max zoom out value. Set to match the width of the stage.
+@export var min_zoom: float = 1.5 # Min zoom in value.
+@export_range(0.1, 2) var zoom_scale: float = 1.2
 var players = []
+var cam_y_pos # y value of the cam's position should be constant.
+var max_distX: float = 2226 # Max distance between players. Set to match the width of the stage.
+var max_distY: float = 1200
 
 @onready var server_player = $"../ServerPlayer"
 @onready var client_player = $"../ClientPlayer"
 
 func _ready():
-	players += [server_player, client_player]
+	initialize_cam()
 
 func _process(_delta):
 	update_position()
 	zoom_camera()
+
+func initialize_cam():
+	players += [server_player, client_player]
+	cam_y_pos = get_limit(SIDE_BOTTOM)
 
 func update_position():
 	# Place cam position to the avg X position of all players
@@ -37,8 +43,17 @@ func zoom_camera():
 			var distY: float = abs(i.position.y - j.position.y)
 			max_distanceX = max(distX, max_distanceX)
 			max_distanceY = max(distY, max_distanceY)
-	max_distanceX = max(min_zoomX, max_distanceX)
-	max_distanceY = max(min_zoomY, max_distanceY)
-	var zoom_amount:float = max(max_distanceX / zoom_scaleX, max_distanceY / zoom_scaleY)
-	zoom_amount = min(max_zoom, zoom_amount)
+	max_distanceX = max(min_distX, max_distanceX)
+	max_distanceY = max(min_distY, max_distanceY)
+	var zoom_amount:float = min(scale_zoomX(max_distanceX), scale_zoomY(max_distanceY))
+	zoom_amount = min(zoom_amount, min_zoom)
+	zoom_amount = max(zoom_amount, max_zoom)
 	self.zoom = Vector2(zoom_amount, zoom_amount)
+
+func scale_zoomX(dist: float) -> float:
+	var val = ((dist - min_distX) / (max_distX - min_distX)) * (max_zoom - min_zoom) + min_zoom
+	return val / zoom_scale
+
+func scale_zoomY(dist: float) -> float:
+	var val = ((dist - min_distY) / (max_distY - min_distY)) * (max_zoom - min_zoom) + min_zoom
+	return val / zoom_scale
